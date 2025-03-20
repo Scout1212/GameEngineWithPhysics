9f081cefad3c9d9c34cfae44422fef823e35be4f
Robert Zamora <RobertZamora.act@gmail.com>
2025-03-20 19:10:17 -0400

    Initial commit

---
+++ src/Runner.java
@@ -0,0 +1,3 @@
+public class Runner {
+    //generate force vectors based on center of mass to mouse click and then calculate the vectors --> set the vectors to variables in object then calculate stuff
+}
